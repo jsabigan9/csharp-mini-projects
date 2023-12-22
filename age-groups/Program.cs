23888da5eab0071b8a8329f4ab2c19d8859b099f
jsabigan9 <jsabigan9@gmail.com>
2023-12-22 16:29:59 +0800

    added age-group

---
+++ age-groups/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace age_groups;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Determine if you're and Child, Teenager, Adult, or Senior!");
+
+        Console.Write("Enter your Name: ");
+        string nameInput = Console.ReadLine();
+
+        Console.Write("Enter your Age: ");
+        int ageInput = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\n -_-_-_-_-_-_-_-_-_-_-_-_-_- \n");
+
+        
+        if (ageInput < 12)
+        {
+            Console.WriteLine($"Welcome {nameInput.ToUpper()}, You're a Child.");
+        }
+        else if ((ageInput >= 13) && (ageInput <= 19))
+        {
+            Console.WriteLine($"Welcome {nameInput.ToUpper()}, You're a Teenager.");
+        }
+        else if ((ageInput >= 20) && (ageInput <= 64))
+        {
+            Console.WriteLine($"Welcome {nameInput.ToUpper()}, You're an Adult.");
+        }
+        else
+        {
+            Console.WriteLine($"Welcome {nameInput.ToUpper()}, You're a Senior.");
+        }
+
+
+        Console.ReadKey();
+    }
+}
