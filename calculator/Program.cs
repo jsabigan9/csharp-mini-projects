5d7bd8099feb7196318ce971040b340498c49da6
jsabigan9 <jsabigan9@gmail.com>
2023-12-25 16:02:02 +0800

    added calculator

---
+++ calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace calculator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Enter num1: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Operator (+ - * /): ");
+        char op = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Enter num2: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        switch (op)
+        {
+            case '+':
+                Console.WriteLine($"{num1} {op} {num2} = {num1 + num2}");
+                break;
+
+            case '-':
+                Console.WriteLine($"{num1} {op} {num2} = {num1 - num2}");
+                break;
+
+            case '*':
+                Console.WriteLine($"{num1} {op} {num2} = {num1 * num2}");
+                break;
+
+            case '/':
+                Console.WriteLine($"{num1} {op} {num2} = {num1 / num2}");
+                break;
+
+            default:
+                Console.WriteLine("Error: Invalid operator entered.");
+                break;
+        }
+    }
+}
---
+++ calculator/readme.md
@@ -0,0 +1,17 @@
+﻿# Calculator
+
+## Exercise:
+This project is designed to help beginners practice using the `switch` statement in C#.
+
+## Overview:
+The Calculator project is a simple console application that performs basic arithmetic operations based on user input. It allows the user to input two numbers and an operator (+, -, *, /), and then it performs the corresponding calculation using a `switch` statement.
+
+## Project Description:
+The program prompts the user to enter the first number, choose an operator, and enter the second number. It then uses a `switch` statement to determine the operation to perform and displays the result. Additionally, the program handles division by zero by checking if the divisor is zero before performing the division.
+
+## How to Use:
+1. Run the program.
+2. Enter the first number when prompted.
+3. Enter an operator (+, -, *, /) when prompted.
+4. Enter the second number when prompted.
+5. The program will display the result of the calculation.
