616d03c4740e2d4e7fbaac7b6a4fdac9ddd1fbff
jsabigan9 <jsabigan9@gmail.com>
2024-01-01 16:24:59 +0800

    added factorial-calculator

---
+++ factorial-calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        do
+        {
+            Console.WriteLine("Factorial Calculator!\n");
+            Console.Write("Enter a whole number: ");
+            int numInput = Convert.ToInt32(Console.ReadLine());
+
+            int factorialNum = 1;
+            for (int index = 1; index <= numInput; index++)
+            {
+                factorialNum *= index;
+            }
+            Console.WriteLine($"\nThe Factorial of {numInput} is: {factorialNum}.");
+
+            Console.WriteLine("Press any key to calculate again...");
+            Console.ReadKey();
+            Console.Clear();
+        } while (true);
+
+    }
+}
