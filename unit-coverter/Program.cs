ccc8a0703e7bbc4823b64ae0a11a05f5a66f2aa8
jsabigan9 <jsabigan9@gmail.com>
2024-01-13 15:21:12 +0800

    add cm, m, in convertion

--- unit-coverter/Program.cs
+++ unit-coverter/Program.cs
@@ -3,49 +3,52 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        StartProgram();
+        MainMenu();
     }
 
     #region Start Program
-    static void StartProgram()
+    static void MainMenu()
     {
         DisplayMenu("Welcome to the Unit Converter!\n");
         DisplayMenu("(1) Feet Converter");
-        DisplayMenu("(2) Exit the program");
+        DisplayMenu("(2) Inch Converter");
+        DisplayMenu("(3) Centimeter Converter");
+        DisplayMenu("(4) Meter Converter");
+        DisplayMenu("(5) Exit the program\n");
 
         while (true)
         {
             string stringInput = Console.ReadLine();
             int intInput;
 
-            if (!int.TryParse(stringInput, out intInput) || intInput < 1 || intInput > 2)
+            if (!int.TryParse(stringInput, out intInput) || intInput < 1 || intInput > 5)
             {
-                DisplayErrorMessage("Invalid choice. Please enter 1 or 2.");
+                DisplayError("Invalid choice. Please enter 1, 2, 3, 4 or 5.");
                 continue;
             }
 
             switch (intInput)
             {
                 case 1: Console.Clear(); FeetConverter(); return;
-                case 2: DisplayThankyouMessage("\nThank you for using the Unit Converter!"); return;
+                case 2: Console.Clear(); InchConverter(); return;
+                case 3: Console.Clear(); CentimeterConverter(); return;
+                case 4: Console.Clear(); MeterConverter(); return;
+                case 5: Console.WriteLine("\nThank you for using the Unit Converter!"); return;
                 default: return;
             }
         }
     }
     #endregion
 
+
     #region FeetConverter
     static void FeetConverter()
     {
-        const double InchesPerFoot = 12;
-        const double CentimetersPerFoot = 30.48;
-        const double MetersPerFoot = 0.3048;
+
 
         DisplayMenu("Welcome to the Feet Converter!\n");
-        DisplayMenu("(1) Convert to inches");
-        DisplayMenu("(2) Convert to centimeters");
-        DisplayMenu("(3) Convert to meters");
-        DisplayMenu("(4) Back to Main Menu");
+        DisplayMenu("(1) Convert Feet Value");
+        DisplayMenu("(2) Back to Main Menu");
 
         while (true)
         {
@@ -55,50 +58,53 @@ internal class Program
 
             if (!int.TryParse(stringInput, out intInput) || intInput < 1 || intInput > 4)
             {
-                DisplayErrorMessage("Invalid choice. Please enter 1, 2, 3, or 4!!!\n");
+                DisplayError("Invalid choice. Please enter 1, 2, 3, or 4!!!\n");
                 continue;
             }
 
             switch (intInput)
             {
-                case 1: FeetToInchConvertion(); return;
-                case 2: return;
-                case 3: return;
-                case 4: Console.Clear(); StartProgram(); return;
+                case 1: FeetConvertion(); return;
+                case 2: Console.Clear(); MainMenu(); return;
                 default: return;
             }
         }
-        static void FeetToInchConvertion()
+        static void FeetConvertion()
         {
-            const double FeetToInch = 12;
+            const double InchesPerFoot = 12;
+            const double CentimetersPerFoot = 30.48;
+            const double MetersPerFoot = 0.3048;
 
             while (true)
             {
                 Console.Clear();
-                DisplayMenu("[1] Feet To Inch Converter\n");
+                DisplayMenu("FEET CONVERTER\n");
                 var feetInput = GetFeetInput();
 
                 Console.Clear();
-                DisplayResultMessage($"FEET: {feetInput}");
-                DisplayResultMessage($"INCH: {feetInput * FeetToInch:F2}");
+                DisplayResult($"ft: {feetInput}\n");
+                DisplayResult($"in: {feetInput * InchesPerFoot:F2}");
+                DisplayResult($"cm: {feetInput * CentimetersPerFoot:F2}");
+                DisplayResult($"m: {feetInput * MetersPerFoot:F2}");
 
                 DisplayMenu("\n(1) Convert Another Value");
-                DisplayMenu("(2) Back To Previos Main Menu");
-                DisplayMenu("(3) Back To Main Menu");
+                DisplayMenu("(2) Back To Main Menu");
 
-                string input = Console.ReadLine().ToLower();
+                while (true)
+                {
+                    string input = Console.ReadLine().ToLower();
+                    if (!int.TryParse(input, out int intInput) || (intInput < 1 || intInput > 2))
+                    {
+                        DisplayError("Please enter 1 or 2");
+                        continue;
+                    }
 
-                if ((!int.TryParse(input, out int intInput)) || (intInput < 1 && intInput > 3))
-                {
-                    DisplayErrorMessage("Please enter a 1, 2, or 3!\n");
-                    continue;
-                }
-                switch (intInput)
-                {
-                    case 1: Console.Clear(); FeetToInchConvertion(); return;
-                    case 2: Console.Clear(); FeetConverter(); return;
-                    case 3: Console.Clear(); StartProgram(); return;
-                    default: return;
+                    switch (intInput)
+                    {
+                        case 1: Console.Clear(); FeetConvertion(); return;
+                        case 2: Console.Clear(); MainMenu(); return;
+                        default: return;
+                    }
                 }
             }
 
@@ -111,7 +117,7 @@ internal class Program
 
                     if ((!double.TryParse(inputString, out double intInput)) || intInput <= 0)
                     {
-                        DisplayErrorMessage("Please enter a valid positive numeric input.\n");
+                        DisplayError("Please enter a valid positive numeric input.\n");
                         continue;
                     }
                     return intInput;
@@ -121,24 +127,283 @@ internal class Program
     }
     #endregion
 
+    #region InchConverter
+    static void InchConverter()
+    {
+
+
+        DisplayMenu("Welcome to the Inch Converter!\n");
+        DisplayMenu("(1) Convert Inch Value");
+        DisplayMenu("(2) Back to Main Menu");
+
+        while (true)
+        {
+
+            string stringInput = Console.ReadLine();
+            int intInput;
+
+            if (!int.TryParse(stringInput, out intInput) || intInput < 1 || intInput > 4)
+            {
+                DisplayError("Invalid choice. Please enter 1 or 2!!!\n");
+                continue;
+            }
+
+            switch (intInput)
+            {
+                case 1: InchConvertion(); return;
+                case 2: Console.Clear(); MainMenu(); return;
+                default: return;
+            }
+        }
+        static void InchConvertion()
+        {
+            const double FootPerInch = 0.083333;
+            const double CentimetersPerInch = 2.54;
+            const double MetersPerInch = 0.0254;
+
+            while (true)
+            {
+                Console.Clear();
+                DisplayMenu("INCH CONVERTER\n");
+                var inchInput = GetInchInput();
+
+                Console.Clear();
+                DisplayResult($"in: {inchInput}\n");
+
+                DisplayResult($"ft: {inchInput * FootPerInch:F4}");
+                DisplayResult($"cm: {inchInput * CentimetersPerInch:F4}");
+                DisplayResult($"m: {inchInput * MetersPerInch:F4}");
+
+                DisplayMenu("\n(1) Convert Another Value");
+                DisplayMenu("(2) Back To Main Menu");
+
+                while (true)
+                {
+                    string input = Console.ReadLine().ToLower();
+                    if (!int.TryParse(input, out int intInput) || (intInput < 1 || intInput > 2))
+                    {
+                        DisplayError("Please enter 1 or 2");
+                        continue;
+                    }
+
+                    switch (intInput)
+                    {
+                        case 1: Console.Clear(); InchConvertion(); return;
+                        case 2: Console.Clear(); MainMenu(); return;
+                        default: return;
+                    }
+                }
+            }
+
+            static double GetInchInput()
+            {
+                while (true)
+                {
+                    Console.Write("Enter Inch Value: ");
+                    string inputString = Console.ReadLine();
+
+                    if ((!double.TryParse(inputString, out double intInput)) || intInput <= 0)
+                    {
+                        DisplayError("Please enter a valid positive numeric input.\n");
+                        continue;
+                    }
+                    return intInput;
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region CentimeterConverter
+    static void CentimeterConverter()
+    {
+
+
+        DisplayMenu("Welcome to the Centimeter Converter!\n");
+        DisplayMenu("(1) Convert Centimeter Value");
+        DisplayMenu("(2) Back to Main Menu");
+
+        while (true)
+        {
+
+            string stringInput = Console.ReadLine();
+            int intInput;
+
+            if (!int.TryParse(stringInput, out intInput) || intInput < 1 || intInput > 4)
+            {
+                DisplayError("Invalid choice. Please enter 1 or 2!!!\n");
+                continue;
+            }
+
+            switch (intInput)
+            {
+                case 1: CentimeterConvertion(); return;
+                case 2: Console.Clear(); MainMenu(); return;
+                default: return;
+            }
+        }
+        static void CentimeterConvertion()
+        {
+            const double CentimeterPerFoot = 30.48;
+            const double CentimeterPerInch = 2.54;
+            const double CentimeterPerMeter = 100;
+
+            while (true)
+            {
+                Console.Clear();
+                DisplayMenu("CENTIMETER CONVERTER\n");
+                var centimeterInput = GetCentimeterInput();
+
+                Console.Clear();
+                DisplayResult($"cm: {centimeterInput}\n");
+
+                DisplayResult($"ft: {centimeterInput / CentimeterPerFoot:F4}");
+                DisplayResult($"in: {centimeterInput / CentimeterPerInch:F4}");
+                DisplayResult($"m: {centimeterInput / CentimeterPerMeter:F4}");
+
+                DisplayMenu("\n(1) Convert Another Value");
+                DisplayMenu("(2) Back To Main Menu");
+
+                while (true)
+                {
+                    string input = Console.ReadLine().ToLower();
+                    if (!int.TryParse(input, out int intInput) || (intInput < 1 || intInput > 2))
+                    {
+                        DisplayError("Please enter 1 or 2");
+                        continue;
+                    }
+
+                    switch (intInput)
+                    {
+                        case 1: Console.Clear(); CentimeterConvertion(); return;
+                        case 2: Console.Clear(); MainMenu(); return;
+                        default: return;
+                    }
+                }
+            }
+
+            static double GetCentimeterInput()
+            {
+                while (true)
+                {
+                    Console.Write("Enter Centimeter Value: ");
+                    string inputString = Console.ReadLine();
+
+                    if ((!double.TryParse(inputString, out double intInput)) || intInput <= 0)
+                    {
+                        DisplayError("Please enter a valid positive numeric input.\n");
+                        continue;
+                    }
+                    return intInput;
+                }
+            }
+        }
+    }
+
+    #endregion
+
+    #region MeterConverter
+    static void MeterConverter()
+    {
+
+
+        DisplayMenu("Welcome to the Meter Converter!\n");
+        DisplayMenu("(1) Convert Meter Value");
+        DisplayMenu("(2) Back to Main Menu");
+
+        while (true)
+        {
+
+            string stringInput = Console.ReadLine();
+            int intInput;
+
+            if (!int.TryParse(stringInput, out intInput) || intInput < 1 || intInput > 4)
+            {
+                DisplayError("Invalid choice. Please enter 1 or 2!!!\n");
+                continue;
+            }
+
+            switch (intInput)
+            {
+                case 1: MeterConvertion(); return;
+                case 2: Console.Clear(); MainMenu(); return;
+                default: return;
+            }
+        }
+        static void MeterConvertion()
+        {
+            const double MeterPerFoot = .3048;
+            const double MeterPerInch = 0.0254;
+            const double MeterPerCentimeter = 0.01;
+
+            while (true)
+            {
+                Console.Clear();
+                DisplayMenu("METER CONVERTER\n");
+                var meterInput = GetMeterInput();
+
+                Console.Clear();
+                DisplayResult($"m: {meterInput}\n");
+
+                DisplayResult($"ft: {meterInput / MeterPerFoot:F2}");
+                DisplayResult($"in: {meterInput / MeterPerInch:F2}");
+                DisplayResult($"cm: {meterInput / MeterPerCentimeter:F2}");
+
+                DisplayMenu("\n(1) Convert Another Value");
+                DisplayMenu("(2) Back To Main Menu");
+
+                while (true)
+                {
+                    string input = Console.ReadLine().ToLower();
+                    if (!int.TryParse(input, out int intInput) || (intInput < 1 || intInput > 2))
+                    {
+                        DisplayError("Please enter 1 or 2");
+                        continue;
+                    }
+
+                    switch (intInput)
+                    {
+                        case 1: Console.Clear(); MeterConvertion(); return;
+                        case 2: Console.Clear(); MainMenu(); return;
+                        default: return;
+                    }
+                }
+            }
+
+            static double GetMeterInput()
+            {
+                while (true)
+                {
+                    Console.Write("Enter Meter Value: ");
+                    string inputString = Console.ReadLine();
+
+                    if ((!double.TryParse(inputString, out double intInput)) || intInput <= 0)
+                    {
+                        DisplayError("Please enter a valid positive numeric input.\n");
+                        continue;
+                    }
+                    return intInput;
+                }
+            }
+        }
+    }
+
+    #endregion
+
+
     #region Messages
     static void DisplayMenu(string message)
     {
         Console.WriteLine(message);
     }
-    static void DisplayResultMessage(string message)
+    static void DisplayResult(string message)
     {
         Console.WriteLine(message);
     }
 
-    static void DisplayErrorMessage(string message)
+    static void DisplayError(string message)
     {
         Console.WriteLine($"\nError: {message}\n");
-    }
-
-    static void DisplayThankyouMessage(string message)
-    {
-        Console.WriteLine($"\n{message}\n");
     }
     #endregion
 
