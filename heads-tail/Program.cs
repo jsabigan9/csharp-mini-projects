91930312648dbb422e1fb5bfe2d3a3c6cad0a84a
jsabigan9 <jsabigan9@gmail.com>
2024-01-20 15:27:44 +0800

    heads tail mini game

--- heads-tail/Program.cs
+++ heads-tail/Program.cs
@@ -1,3 +1,17 @@
 ﻿Random coin = new Random();
 int flip = coin.Next(0, 2);
-Console.WriteLine((flip == 0) ? "heads" : "tails");
+//.WriteLine((flip == 0) ? "heads" : "tails");
+Console.WriteLine("Heads or Tails?");
+
+Console.Write("Enter your guess: ");
+var guess = Console.ReadLine();
+
+var flipResult = flip == 0 ? "heads" : "tails";
+
+Console.WriteLine($"\nFlip result: {flipResult}");
+Console.WriteLine($"Your guess: {guess}");
+
+if (guess.ToLower() == flipResult)
+    Console.WriteLine("You win!");
+else
+    Console.WriteLine("You lose!");
