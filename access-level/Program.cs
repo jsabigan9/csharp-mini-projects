522e6b614926ed8e027ae682610ed0d56304a0db
jsabigan9 <jsabigan9@gmail.com>
2024-01-21 15:27:27 +0800

    added access level exercise from microsoft learn

---
+++ access-level/Program.cs
@@ -0,0 +1,28 @@
+﻿string permissionAccess;
+int accessLevel;
+
+Console.Write("Enter your Permission Access [ Admin | Manager ]: ");
+permissionAccess = Console.ReadLine().ToUpper();
+
+Console.Write("Enter your Access Level: ");
+accessLevel = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (permissionAccess.Contains("ADMIN"))
+{
+    if (accessLevel > 55)
+        Console.WriteLine("Welcome, Super Admin user.");
+    else if (accessLevel <= 55)
+        Console.WriteLine("Welcome, Admin user.");
+}
+else if (permissionAccess.Contains("MANAGER"))
+{
+    if (accessLevel >= 20)
+        Console.WriteLine("Contact an Admin for access.");
+    else if (accessLevel < 20)
+        Console.WriteLine("You do not have sufficient privileges.");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
